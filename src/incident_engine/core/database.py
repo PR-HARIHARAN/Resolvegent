51309129801d914945953df31fdc469820ee475f
Amar-7778 <amarnathr267@gmail.com>
2026-09-19 03:29:42 +0530

    chore: add EC2 docker deployment configuration

--- src/incident_engine/core/database.py
+++ src/incident_engine/core/database.py
@@ -2,10 +2,12 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./incidents.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./incidents.db")
+
+connect_args = {"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL, connect_args=connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
